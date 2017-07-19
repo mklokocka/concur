300c05bb798874a11a8d23db0ccabd160301fb78
Mikulas Klokocka <mklokocka@localhost.localdomain>
2017-07-19 20:32:10 +0200

    Changed the base url for the application and made the code a bit more elegant.

--- cli/app.py
+++ cli/app.py
@@ -3,20 +3,7 @@ import requests
 import json
 from collections import OrderedDict
 
-
-# Method defining the url of the host serving the API.
-def url_base():
-    return 'http://localhost:5000'
-
-
-# Helper function for formatting the request URL.
-def format_url(amount, input_currency, output_currency=None):
-    url = url_base() + '/currency_converter?amount=%d&input_currency=%s'\
-                            % (amount, input_currency)
-    if output_currency is not None:
-        url = url + '&output_currency=%s' % output_currency
-
-    return url
+URL_BASE = 'http://localhost:8080/currency_converter'
 
 
 @click.command()
@@ -29,13 +16,20 @@ def convert(amount, input_currency, output_currency):
     if input_currency is None:
         raise click.UsageError('Input currency argument required.')
 
-    resp = requests.get(format_url(amount, input_currency, output_currency))
+    params = {
+        'amount': amount,
+        'input_currency': input_currency
+    }
 
-    # Check for error, inform the user of them.
-    if resp.status_code != 200:
-        if resp.status_code == 400:
-            raise click.UsageError('Did you enter the right amount and '
-                                   'correct currency codes?')
+    if output_currency is not None:
+        params['output_currency'] = output_currency
+
+    try:
+        resp = requests.get(URL_BASE, params=params)
+        resp.raise_for_status()
+    except requests.HTTPError:
+        raise click.UsageError('Did you enter the right amount and '
+                               'correct currency codes?')
 
     # Convert the response text into json with a preserved order for printing.
     ordered_resp = json.loads(resp.text, object_pairs_hook=OrderedDict)
